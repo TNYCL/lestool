e05b0f8b27a05c068fdfbf12e2868f7fd185939f
TNYCL <tunaycl@outlook.com>
2022-04-08 18:40:48 +0300

    gg

--- pixel_cleaner.py
+++ pixel_cleaner.py
@@ -35,59 +35,17 @@ def select_resolution():
         main.error("Wrong Option.\n")
         select_resolution()
 
-def click_event(event, x, y, flags, params):
- 
-    # checking for left mouse clicks
-    if event == cv2.EVENT_LBUTTONDOWN:
- 
-        # displaying the coordinates
-        # on the Shell
-        print(x, ' ', y)
- 
-        # displaying the coordinates
-        # on the image window
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, str(x) + ',' +
-                    str(y), (x,y), font,
-                    1, (255, 0, 0), 2)
-        cv2.imshow('image', img)
- 
-    # checking for right mouse clicks    
-    if event==cv2.EVENT_RBUTTONDOWN:
- 
-        # displaying the coordinates
-        # on the Shell
-        print(x, ' ', y)
- 
-        # displaying the coordinates
-        # on the image window
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        b = img[y, x, 0]
-        g = img[y, x, 1]
-        r = img[y, x, 2]
-        cv2.putText(img, str(b) + ',' +
-                    str(g) + ',' + str(r),
-                    (x,y), font, 1,
-                    (255, 255, 0), 2)
-        cv2.imshow('image', img)
-
-def test():
-    for file in filepath:
-        global img
-        img = cv2.imread(file, 1)
-
-        cv2.imshow('image', img)
-        cv2.setMouseCallback('image', click_event)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
 def res_64x64():
     for file in filepath:
         skin = cv2.imread(file, cv2.IMREAD_COLOR)
 
-        skin[0:8, 15:25] = [0, 0, 0]
+        name = os.path.basename(file)
+        if name.endswith(".png"): name = name.replace(".png", "")
+        else: name = name.replace(".jpg", "")
 
-        cv2.imwrite(file, skin)
+        skin[0:8, 0:8] = (0, 0, 0)
+
+        cv2.imwrite(file.replace(name, name + "_cleared"), skin)
 
 def res_128x128():
     for file in filepath:
